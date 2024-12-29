aaabf90186b7bb1f0ffe47594aa937a6c2275f38
Kevin Gonzalez <Kevinjwgonzalez@gmail.com>
2024-12-29 03:45:27 -0500

    feat: intitialize 2048 app with main grid and funciton placeholders

--- 2048.py
+++ 2048.py
@@ -1,20 +1,66 @@
-class Board:
-    matrix = [[2,4,6,8], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    pass
+from calendar import c
+import curses
+from curses import wrapper
+import time
 
-class Game:
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("------  2048 Test Game  ------")
+    stdscr.addstr(1,0, "--- Click any key to continue ---")
+    stdscr.refresh()
+    stdscr.getkey()
 
-    pass
 
-
-
-def shift():
-    pass
-
-def compress():
-    pass
-
-def spawn():
+def spawn(stdscr):
     pass
 
 
+def shfit(stdscr):
+    pass
+
+
+def compress(stdscr):
+    pass
+
+
+
+
+
+
+def grid(stdscr):
+    matrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    game_state = True
+
+    #spawn function
+
+    while game_state == True:
+        try:
+            direction = stdscr.getkey() # w a s d for valid direction inputs 
+        except:
+            continue    # note this blocks it... like an input... now it does not bc of the nodelay
+
+        #shift
+        #compress
+        #shift again
+        #spawn
+
+
+
+        if ord(direction) == 27: #escape key to leave
+            break
+
+
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+    grid(stdscr)
+
+    
+    
+    
+wrapper(main)
